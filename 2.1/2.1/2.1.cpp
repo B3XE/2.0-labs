b1703a53be0b0109c451c0fd6bc6b56b9bf8b453
B3XE <bogdanw321w@gmail.com>
2023-10-07 10:45:31 +0300

    2.0labs2branch1

--- 2.1/2.1/2.1.cpp
+++ 2.1/2.1/2.1.cpp
@@ -5,9 +5,12 @@
 using namespace std;
 int main()
 {
+    //ініціалізація даних
     double x, z1, z2;
     cout << "x = "; cin >> x;
+    //рівняння один
     z1 = ((x + 2) / sqrt(2 * x) - x / (sqrt(2 * x) + 2) + 2 / (x - sqrt(2 * x))) * (sqrt(x) - sqrt(2)) / (x + 2);
+    //рівняння два
     z2 = 1 / (sqrt(x) + sqrt(2));
     cout << "z1 = " << z1 << endl;
     cout << "z2 = " << z2 << endl;
